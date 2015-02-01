f044f162886d4d1d9e1699781cd0e388d232efc4
khod <donotreply@nadainnovation.ca>
2015-02-01 17:13:08 -0500

    add files

---
+++ js/controllers/contact.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+	var e1 = 'nada',
+		e2 = 'innova',
+		e3 = 'tion.',
+		e4 = 'ca',
+		e5 = '@',
+		e6 = 'gma',
+		e7 = 'il.com';
+
+	var p1 = '+ 1 ',
+		p2 = '(519)',
+		p3 = '772-',
+		p4 = '68',
+		p5 = '47';
+
+
+	$('#e-nada').text(e1+e2+e3+e4+e5+e6+e7);
+	$('#p-nada').text(p1+p2+p3+p4+p5);
+		
+});
